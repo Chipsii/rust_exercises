6889116b6dc719bbfd643af4e3f333a9abd4336b
Chipsii <hasibfire24@gmail.com>
2025-02-27 01:06:28 +0600

    didnt understand destructuring tuples

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    // let (bat_man, super_man) = ('x', 29);
+    // println!("{} {}", bat_man, super_man);
+
+    // let (bat_man_one, super_man_1) = justice(bat_man.to_string(), super_man);
+    // println!("{} {}", bat_man_one, super_man_1);
+
+    // let (_, super_man) = (",", 29);
+    // println!("{}", super_man);
 }
+
+// fn justice(bat_man: String, super_man: i32) -> (String, i32) {
+//     println!("{}{}", bat_man, super_man);
+//     (bat_man, super_man)
+// }
+
+// I didint understand destructuring tuples
