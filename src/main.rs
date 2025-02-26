3c89ad545dbb9f861ac36604b43061b3a5584424
Chipsii <hasibfire24@gmail.com>
2025-02-27 01:25:50 +0600

    statements done but a bit confused

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    let y = 7;
+    let z = x + y;
+    let alpha = value_x(z);
+    println!("The value of alpha is: {}", alpha);
+
+    let x = vec![2, 4, 6].iter().map(|x| x * 2).fold(0, |x, y| x + y);
+    println!("The value of x is: {}", x);
+}
+
+fn value_x(z: i32) -> i32 {
+    println!("The value of x is: {}", z);
+    z
 }
