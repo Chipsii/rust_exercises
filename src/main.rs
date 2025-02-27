a23a8f9181918d27b293bd705c238ce675bf863c
Chipsii <hasibfire24@gmail.com>
2025-02-27 22:53:44 +0600

    functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    let y = 10;
+    let z = x + y;
+    let unreal = engine(z);
+    println!("Hello {}", unreal);
+
+    let x = vec![2, 4, 6].iter().map(|x| x * 5).fold(0, |x, y| x + y);
+    println!("Hello {}", x);
+}
+
+fn engine(z: i32) -> i32 {
+    println!("value is {}", z);
+    z
 }
