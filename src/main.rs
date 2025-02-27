a79e34d5bbe6f832057e641d0248ed30ee33454f
Chipsii <hasibfire24@gmail.com>
2025-02-27 23:14:45 +0600

    blocks are expressions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    println!("Value is: {}", x);
+    let x = { 5 };
+    println!("Value IS: {}", x);
+
+    let x = {
+        let y = 5;
+        let z = 10;
+        y + z
+    };
+    println!("Value of x is: {}", x);
 }
