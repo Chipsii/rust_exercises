99a433a3573156a364de9427899c360c1d6bac9b
Chipsii <hasibfire24@gmail.com>
2025-02-27 23:29:59 +0600

    everything have a sauce

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    println!("Value of x: {}", x);
+    let x = sauce(x);
+    println!("Value of x: {}", x);
+}
+
+fn sauce(x: i32) -> i32 {
+    let spider = x;
+    let x = if spider != 5 {
+        spider
+    } else {
+        spider * 2
+    };
+
+    println!("Value of x: {}", x);
+    x + 1
 }
