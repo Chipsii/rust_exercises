0baf2073f7639ffcbe8f54476d74c915a9a6f4b0
Chipsii <hasibfire24@gmail.com>
2025-02-27 23:07:40 +0600

    blocks

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+
+    let venom = carnage(x);
+    println!("The value of venom is: {}", venom);
+    {
+        let x = vec![1, 2, 3].iter().map(|x| x + 1).fold(0, |x, y| x + y);
+        println!("we are: {}", x);
+    }
+}
+
+fn carnage(x: i32) -> i32 {
+    println!("we will: {}", x);
+    x
 }
