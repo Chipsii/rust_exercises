adcf76b1705239eba3b817010bbdb4a560c26b46
Chipsii <hasibfire24@gmail.com>
2025-03-03 01:33:18 +0600

    types are namespaces too

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::cmp::max;
+
 fn main() {
-    println!("Hello, world!");
+    let cat = std::cmp::min(6, 17);
+    println!("age of cat is {}", cat);
+
+    let dog = max(06, 17);
+    println!("age of dog is {}", dog);
+
+    let name = "Rust".to_string();
+    let age = str::len(&name);
+    println!("{} is {} years old", name, age);
 }
