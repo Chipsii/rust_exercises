3918f83611b3433847f740add9efc60740624ebe
Chipsii <hasibfire24@gmail.com>
2025-03-03 01:36:28 +0600

    remove unused variables and imports from main function

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,4 @@
-use std::cmp::max;
-
 fn main() {
-    let cat = std::cmp::min(6, 17);
-    println!("age of cat is {}", cat);
-
-    let dog = max(06, 17);
-    println!("age of dog is {}", dog);
-
     let name = "Rust".to_string();
     let age = str::len(&name);
     println!("{} is {} years old", name, age);
