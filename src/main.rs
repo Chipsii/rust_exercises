dbd81ebb1ce0b9cadba93ee30bea8cf75cec1ffb
Chipsii <hasibfire24@gmail.com>
2025-03-03 01:15:46 +0600

    field access and method calling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let x = (20, 25);
+    println!("first element is {}", x.0);
+    let idea: String = "Hello sis".to_string();
+    let idea2 = sleep(idea);
+    println!("{}", idea2);
+}
+
+fn sleep(x: String) -> String {
+    let idea2 = x.len().to_string();
+    idea2
 }
