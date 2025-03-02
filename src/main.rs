2baa98db60d4d4c71a8497767595631079974848
Chipsii <hasibfire24@gmail.com>
2025-03-03 01:46:06 +0600

    the libstd prelude

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let shark = "Sammy".to_string();
+    let mut fish: Vec<String> = Vec::new();
+    fish.push(shark);
+    println!("{:?}", fish);
+
+    let mut fish: Vec<String> = std::vec::Vec::new();
+    fish.push("Dory".to_string());
+    println!("{:?}", fish);
 }
